f7d251402279fe4fcb67c962fa8bad721bb4f68f
CaseySmalley <caseysmalley1996@gmail.com>
2019-07-31 22:03:16 +0100

    finished lexer; added helper functions for syntax_analysis

--- slcc.js
+++ slcc.js
@@ -17,9 +17,10 @@ function() {
 	}
 
 	var source_code = null;
-	var symbol_table = null;
-
+	
 	var lex_predefined_tokens = make_string_map(
+		"import",
+		"export",
 		"sizeof",
 		"typedef",
 		"static",
@@ -49,6 +50,8 @@ function() {
 		"default",
 		"continue",
 		"return",
+		",",
+		".",
 		"(",
 		")",
 		"{",
@@ -63,6 +66,7 @@ function() {
 		"|",
 		"^",
 		"~",
+		"->",
 		"||",
 		"&&",
 		"==",
@@ -89,17 +93,23 @@ function() {
 
 	var lex_tokens = null;
 	var lex_current_pos = 0;
-	var lex_current_line = 0;
+	var lex_current_col = 1;
+	var lex_current_line = 1;
 	var lex_token = null;
+	var lex_token_col = 0;
+	var lex_token_line = 0;
 
-	function Token(type,value) {
+	function Token(type,value,col,line) {
 		this.type = type;
 		this.value = value;
+		this.col = col;
+		this.line = line;
 	}
 
 	function lex_error() {
 		resource.error(
-			"slcc lex error %d: %s",
+			"SLCC_LEX_ERROR: col %d line %d, %s",
+			lex_current_col,
 			lex_current_line,
 			resource.format_string.apply(null,arguments)
 		);
@@ -117,10 +127,18 @@ function() {
 		return source_code.substring(lex_current_pos,lex_current_pos + length);
 	}
 
+	function lex_seek_pos(offset) {
+		lex_current_pos += offset;
+		lex_current_col += offset;
+	}
+
 	function lex_get_token(length) {
 		var token_end = lex_current_pos + length;
 		var token = source_code.substring(lex_current_pos,token_end);
 
+		lex_token_col = lex_current_col;
+		lex_token_line = lex_current_line;
+		lex_current_col += length;
 		lex_current_pos = token_end;
 		lex_token = token;
 	}
@@ -149,10 +167,11 @@ function() {
 
 		while(!lex_eof() && (char === " " || char === "	" || lex_is_newline(char))) {
 			if (lex_is_newline(char)) {
+				lex_current_col = 1;
 				++lex_current_line;
 			}
 
-			++lex_current_pos;
+			lex_seek_pos(1);
 			char = lex_peek_char(0);
 		}
 	}
@@ -194,7 +213,21 @@ function() {
 	}
 
 	function lex_get_char_literal() {
+		if (lex_peek_char(0) === "'" && lex_peek_char(2) === "'") {
+			lex_seek_pos(1);
+			lex_get_token(1);
+			lex_seek_pos(1);
 
+			return true;
+		} else if (lex_peek_char(0) === "'" && lex_peek_char(1) === "\\" && lex_peek_char(3) === "'") {
+			lex_seek_pos(1);
+			lex_get_token(2);
+			lex_seek_pos(1);
+
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	function lex_get_string_literal() {
@@ -216,19 +249,83 @@ function() {
 			}
 		} while(!lex_eof() && char !== "\"")
 
-		++lex_current_pos;
+		lex_seek_pos(1);
 		lex_get_token(length - 1);
-		++lex_current_pos;
+		lex_seek_pos(1);
 
 		return true;
 	}
 
-	function lex_get_float_literal() {
+	var LEX_DECIMAL_FIRST = 1;
+	var LEX_DECIMAL_LAST = 2;
+	var LEX_DECIMAL_DONE = 3;
 
+	function lex_has_decimal() {
+		var char = lex_peek_char(0);
+
+		if (!lex_is_num(char)) {
+			return 0;
+		}
+
+		var length = 0;
+		var state = LEX_DECIMAL_FIRST;
+		
+		do {
+			char = lex_peek_char(++length);
+
+			switch(state) {
+				case LEX_DECIMAL_FIRST:
+					if (char === ".") {
+						if (lex_is_num(lex_peek_char(length + 1))) {
+							state = LEX_DECIMAL_LAST;
+						} else {
+							return 0;
+						}
+					} else if (!lex_is_num(char)) {
+						return 0;
+					}
+				break;
+
+				case LEX_DECIMAL_LAST:
+					if (!lex_is_num(char)) {
+						state = LEX_DECIMAL_DONE;
+					}
+				break;
+			}
+		} while(!lex_eof() && state !== LEX_DECIMAL_DONE)
+
+		return length;
+	}
+
+	function lex_get_float_literal() {
+		var length = lex_has_decimal();
+
+		if (!length) {
+			return false;
+		}
+
+		if (lex_peek_char(length) !== "f") {
+			return false;
+		}
+
+		lex_get_token(length);
+		lex_seek_pos(1);
+		lex_token = parseFloat(lex_token);
+
+		return true;
 	}
 
 	function lex_get_double_literal() {
-		
+		var length = lex_has_decimal();
+
+		if (!length) {
+			return false;
+		}
+
+		lex_get_token(length);
+		lex_token = parseFloat(lex_token);
+
+		return true;
 	}
 
 	function lex_get_integer_literal() {
@@ -256,8 +353,6 @@ function() {
 	}
 
 	function lexical_analysis() {
-		//debugger;
-
 		lex_skip_whitespace();
 
 		while (!lex_eof()) {
@@ -273,19 +368,129 @@ function() {
 				type = lex_token;
 			} else if (lex_get_string_literal()) {
 				type = "string_literal";
+			} else if (lex_get_char_literal()) {
+				type = "char_literal";
+			} else if (lex_get_float_literal()) {
+				type = "float_literal";
+			} else if (lex_get_double_literal()) {
+				type = "double_literal";
 			} else if (lex_get_integer_literal()) {
 				type = "integer_literal";
 			} else {
 				lex_error("unrecognised character '%c'",lex_peek_char(0));
 			}
 
-			lex_tokens.push(new Token(type,lex_token));
+			lex_tokens.push(new Token(type,lex_token,lex_token_col,lex_token_line));
 			lex_skip_whitespace();
 		}
 	}
 
-	function syntax_analysis() {
+	var syn_ast_root = null;
+	var syn_symbol_table = null;
+	var syn_current_pos = 0;
+	var syn_current_pos_save = 0;
 
+	function Ast_Node(type,value,children) {
+		this.type = type;
+		this.value = value;
+		this.children = children || [];
+	}
+
+	function syn_error() {
+		var token = syn_peek_token(0);
+
+		resource.error(
+			"SLCC_SYNTAX_ERROR: col %d line %d, %s",
+			token.col,
+			token.line,
+			resource.format_string.apply(null,arguments)
+		);
+	}
+
+	function syn_eof() {
+		return syn_current_pos === lex_tokens.length;
+	}
+
+	function syn_seek_pos(offset) {
+		syn_current_pos += offset;
+	}
+
+	function syn_save_pos() {
+		syn_current_pos_save = syn_current_pos;
+	}
+
+	function syn_load_pos() {
+		syn_current_pos = syn_current_pos_save;
+	}
+	
+	function syn_peek_token(offset) {
+		return lex_tokens[syn_current_pos + offset];
+	}
+	
+	function syn_expect(type) {
+		var token = syn_peek_token(0);
+
+		if (token.type !== type) {
+			syn_error("expected a '%s' got a '%s' instead",type,token.type);
+		}
+
+		syn_seek_pos(1);
+
+		return token;
+	}
+
+	function syn_accept(type) {
+		var token = syn_peek_token(0);
+
+		if (token.type !== type) {
+			return null;
+		}
+
+		syn_seek_pos(1);
+
+		return token;
+	}
+
+	function syn_declaration() {
+		
+	}
+
+	function syn_function_definition() {
+		
+	}
+
+	function syn_external_declaration() {
+		syn_save_pos();
+
+		var node = syn_function_definition();
+
+		if (node) {
+			return node;
+		}
+
+		syn_load_pos();
+		node = syn_declaration();
+
+		if (node) {
+			return node;
+		} else {
+			syn_load_pos();
+			syn_error("invalid external declaration");
+		}
+	}
+
+	function syn_translation_unit() {
+		var node = new Ast_Node("translation_unit");
+		
+		while (!syn_eof()) {
+			node.children.push(syn_external_declaration());
+		}
+
+		return node
+	}
+
+	function syntax_analysis() {
+		syn_ast_root = syn_translation_unit();
 	}
 
 	function semantic_analysis() {
@@ -302,10 +507,16 @@ function() {
 
 	function reset_vars(src) {
 		source_code = src;
-		symbol_table = {};
+		
 		lex_tokens = [];
 		lex_current_pos = 0;
-		lex_current_line = 0;
+		lex_current_col = 1;
+		lex_current_line = 1;
+
+		syn_ast_root = null;
+		syn_symbol_table = null;
+		syn_current_pos = 0;
+		syn_current_pos_save = 0;
 	}
 
 	return {
@@ -314,9 +525,12 @@ function() {
 
 		parse: function(src) {
 			reset_vars(src);
-			lexical_analysis();
 
-			return lex_tokens;
+			lexical_analysis();
+			syntax_analysis();
+			semantic_analysis();
+
+			return syn_ast_root;
 		},
 
 		compile: function(target,src) {
